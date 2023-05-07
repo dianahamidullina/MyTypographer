bc0a5e112e2333d6480d651fb16c8ab7b3179304
Diana <dianahamidullina14@gmail.com>
2023-05-07 23:16:54 +0300

    \ \

--- MyTypographer/Form1.Designer.cs
+++ MyTypographer/Form1.Designer.cs
@@ -79,7 +79,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.button2.Location = new System.Drawing.Point(512, 241);
+            this.button2.Location = new System.Drawing.Point(512, 421);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 38);
             this.button2.TabIndex = 3;
